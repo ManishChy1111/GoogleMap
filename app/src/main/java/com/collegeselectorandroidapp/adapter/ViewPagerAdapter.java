98b7c028224de1c937fed5b494fb25633b22ffc2
ManishChy1111 <ManishChy1111@gmail.com>
2019-07-11 16:18:18 +0545

    add document in Readme.md

---
+++ app/src/main/java/com/collegeselectorandroidapp/adapter/ViewPagerAdapter.java
@@ -0,0 +1,21 @@
+package adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
